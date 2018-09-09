1eb844b95d254547e004d21a833c2196fcbe4ff5
Jeff Wentworth <jeff@curvegrid.com>
2018-09-09 12:00:11 +0900

    up to 21

--- one-to-ten.go
+++ one-to-ten.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"strconv"
 	"os/exec"
 	"os"
 	"bufio"
@@ -30,8 +31,34 @@ func inLine() string {
 func main() {
 	// setup
 	scanner = bufio.NewScanner(os.Stdin)
-    
-	outLine("2 + 2")
-	line := inLine()
-	outLine(line)
+	
+	// introduction
+	outLine("Welcome to the one to ten game. Good luck!")
+	outLine("What is your name?")
+	name := inLine()
+	outLine("Let's begin " + name)
+	points := 0
+
+	// game loop
+	for a := 1; a <= 21; a++ {
+		outLine(fmt.Sprintf("What is %v + %v?", a, a))
+		answer := inLine()
+		rightAnswer := a + a
+
+		number, err := strconv.Atoi(answer)
+		if err != nil {
+			outLine("That's not a number, " + name + ", please enter a number!")
+			continue
+		}
+
+		if number == rightAnswer {
+			outLine("Good job " + name + ", that is correct!")
+			points++
+		} else {
+			outLine(fmt.Sprintf("Sorry, %v, that is the wrong answer. The right answer is %v", name, rightAnswer))
+		}
+	}
+
+	// print result
+	outLine(fmt.Sprintf("Congratulations %v you scored %v points!", name, points))
 }
